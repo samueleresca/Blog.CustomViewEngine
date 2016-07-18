d5ae109480e8627000641c7a30ad7f7bac34cf40
Samuele Resca <samuele.resca@gmail.com>
2016-07-18 19:59:09 +0200

    Conditional rendering on name

--- Core/CustomViewEngine.cs
+++ Core/CustomViewEngine.cs
@@ -1,32 +1,46 @@
 using Microsoft.AspNetCore.Mvc.Razor;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System.Text.Encodings.Web;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Core
 {
     public class CustomViewEngine : RazorViewEngine
-{
-    public CustomViewEngine( IRazorPageFactoryProvider pageFactory,
-            IRazorPageActivator pageActivator,
-            System.Text.Encodings.Web.HtmlEncoder htmlEncoder,
-            Microsoft.Extensions.Options.IOptions<RazorViewEngineOptions> optionsAccessor,
-            Microsoft.Extensions.Logging.ILoggerFactory loggerFactory) 
-        : base(pageFactory, 
-              pageActivator, 
-              htmlEncoder, optionsAccessor, loggerFactory
-              )
     {
-    }
-
-    public override IEnumerable<string> ViewLocationFormats
-    {
-        get
+        public CustomViewEngine(IRazorPageFactoryProvider pageFactory,
+                IRazorPageActivator pageActivator,
+                HtmlEncoder htmlEncoder,
+                IOptions<RazorViewEngineOptions> optionsAccessor,
+                ILoggerFactory loggerFactory)
+            : base(pageFactory,
+                  pageActivator,
+                  htmlEncoder, optionsAccessor, loggerFactory
+                  )
         {
-            var origLocations= base.ViewLocationFormats;
-            return  base.ViewLocationFormats.Select(f => f.Replace(".cshtml", ".theme.cshtml")).Concat(origLocations);
         }
-    }
 
-}
+        public override IEnumerable<string> ViewLocationFormats
+        {
+            get
+            {
+                var origLocations = base.ViewLocationFormats;
+
+                /*
+                * TODO: Replace with real-world condition
+                */
+                var rand = new Random();
+                var randomCondition = (rand.Next(0, 2) == 0);
+
+
+                var varyExtension = randomCondition ? ".vary1" : ".vary2";
+
+
+                return base.ViewLocationFormats.Select(f => f.Replace(".cshtml", varyExtension + ".cshtml")).Concat(origLocations);
+            }
+        }
+
+    }
 }
